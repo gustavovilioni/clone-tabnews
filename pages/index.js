e518748151fad0812eebe9e2c1ac14492ab741ff
Gustavo Vilioni <60745816+gustavovilioni@users.noreply.github.com>
2023-08-04 20:25:29 +0000

    modifica a frase

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,7 @@
 function Home() {
-    return <h1>Será que vai dar certo essa história de programador?</h1>
+    return <h1>Será que vai dar certo essa história de programador?
+        Não sei não kkkkk
+    </h1>
 }
 
 export default Home;
